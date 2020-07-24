be523fe51d6914e00ac5f45601d63b5bd4e4ef26
faruk-ahmad <faruk.csebrur@gmail.com>
2020-07-24 19:03:24 +0900

    added pagination

--- app/routes.py
+++ app/routes.py
@@ -32,9 +32,14 @@ def index():
         flash('You post is now live!')
         return redirect(url_for('index'))
 
-    posts = current_user.followed_post().all()
+    page = request.args.get('page', 1, type=int)
+    posts = current_user.followed_post().paginate(
+        page, app.config['POSTS_PER_PAGE'], False
+    )
+    next_url = url_for('index', page=posts.next_num) if posts.has_next else None
+    prev_url = url_for('index', page=posts.prev_num) if posts.has_prev else None
 
-    return render_template('index.html', title='Home Page', form=form, posts=posts)
+    return render_template('index.html', title='Home Page', form=form, posts=posts.items, next_url=next_url, prev_url=prev_url)
 
 
 @app.route('/login', methods=['GET', 'POST'])
@@ -164,5 +169,12 @@ def unfollow(username):
 @app.route('/explore')
 @login_required
 def explore():
-    posts = Post.query.order_by(Post.timestamp.desc()).all()
-    return render_template('index.html', title='Explore', posts=posts)
+    page = request.args.get('page', 1, type=int)
+    posts = Post.query.order_by(Post.timestamp.desc()).paginate(
+        page, app.config['POSTS_PER_PAGE'], False
+    )
+
+    next_url = url_for('explore', page=posts.next_num) if posts.has_next else None
+    prev_url = url_for('explore', page=posts.prev_num) if posts.has_prev else None
+
+    return render_template('index.html', title='Explore', posts=posts.items, next_url=next_url, prev_url=prev_url)
