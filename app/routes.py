59b321373f2a820775ca0aae3e6593210e634935
faruk-ahmad <faruk.csebrur@gmail.com>
2020-07-24 18:29:09 +0900

    added follow and unfollow option

--- app/routes.py
+++ app/routes.py
@@ -3,12 +3,12 @@ from app import app
 from app.forms import LoginForm
 
 from flask_login import current_user, login_user, logout_user, login_required
-from app.models import User
+from app.models import User, Post
 
 from werkzeug.urls import url_parse
 
 from app import db
-from app.forms import RegistrationForm, EditProfileForm
+from app.forms import RegistrationForm, EditProfileForm, PostForm, EmptyForm
 from datetime import datetime
 
 
@@ -19,26 +19,22 @@ def before_request():
         db.session.commit()
 
 
-@app.route('/')
-@app.route('/index')
+@app.route('/', methods=['GET', 'POST'])
+@app.route('/index', methods=['GET', 'POST'])
 @login_required
 def index():
-    posts = [
-        {
-            'author': {'username': 'John'},
-            'body': 'Beautiful day in Portland!'
-        },
-        {
-            'author': {'username': 'Susan'},
-            'body': 'The Avengers movie was so cool!'
-        },
-        {
-            'author': {'username': 'Shafi'},
-            'body': 'The website looks cool!'
-        }
+    form = PostForm()
+    if form.validate_on_submit():
+        post = Post(body=form.post.data, author=current_user)
+        db.session.add(post)
+        db.session.commit()
 
-    ]
-    return render_template('index.html', title='Home Page', posts=posts)
+        flash('You post is now live!')
+        return redirect(url_for('index'))
+
+    posts = current_user.followed_post().all()
+
+    return render_template('index.html', title='Home Page', form=form, posts=posts)
 
 
 @app.route('/login', methods=['GET', 'POST'])
@@ -99,13 +95,14 @@ def user(username):
         {'author': user, 'body': 'Test post #2'}
     ]
 
-    return render_template('user.html', user=user, posts=posts)
+    form = EmptyForm()
+    return render_template('user.html', user=user, posts=posts, form=form)
 
 
 @app.route('/edit_profile', methods=['GET', 'POST'])
 @login_required
 def edit_profile():
-    form = EditProfileForm()
+    form = EditProfileForm(current_user.username)
 
     if form.validate_on_submit():
         current_user.username = form.username.data
@@ -120,3 +117,45 @@ def edit_profile():
         form.about_me.data = current_user.about_me
     
     return render_template('edit_profile.html', title='Edit Profile', form=form)
+
+
+@app.route('/follow/<username>', methods=['POST'])
+@login_required
+def follow(username):
+    form = EmptyForm()
+
+    if form.validate_on_submit():
+        user = User.query.filter_by(username=username).first()
+        if user is None:
+            flash('User {} not found'.format(username))
+            return redirect(url_for('index'))
+        if user == current_user:
+            flash('You can not follow yourself!')
+            return redirect(url_for('user', username=username))
+        current_user.follow(user)
+        db.session.commit()
+        flash('You are following {} now!'.format(username))
+        return redirect(url_for('user', username=username))
+    else:
+        return redirect(url_for('index'))
+
+@app.route('/unfollow/<username>', methods=['POST'])
+@login_required
+def unfollow(username):
+    form = EmptyForm()
+
+    if form.validate_on_submit():
+        user = User.query.filter_by(username=username).first()
+        if user is None:
+            flash('User {} not found.'.format(username))
+            return redirect(url_for('index'))
+        if user == current_user:
+            flash('You can not unfollow yourself!')
+            return redirect(url_for('user', username=username))
+        
+        current_user.unfollow(user)
+        db.session.commit()
+        flash('You are not following {} now!'.format(username))
+        return redirect(url_for('user', username=username))
+    else:
+        return redirect(url_for('index'))
